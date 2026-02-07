b9ee6ead4a26d57ef81cc92a9d19d0f8850583f7
Alden G <aldengrover@gmail.com>
2026-02-07 10:15:04 -0500

    updated detector decleration

--- apriltag_homography.py
+++ apriltag_homography.py
@@ -19,7 +19,14 @@ def rectify_image_with_apriltag(image_path, tag_family='tag36h11', tag_size_pixe
         return
 
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    detector = apriltag.Detector(options=apriltag.DetectorOptions(families=tag_family))
+    detector = Detector(
+            families=tag_family,
+            nthreads=1,
+            quad_decimate=1.0,
+            quad_sigma=0.0,
+            refine_edges=1,
+            decode_sharpening=0.25,
+            debug=0)
     results = detector.detect(gray)
 
     if not results:
