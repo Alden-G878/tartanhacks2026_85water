ba6344fa89cd79f719868173e41b6bd0479f03a6
Alden G <aldengrover@gmail.com>
2026-02-07 11:41:17 -0500

    Updated script

--- camera_undistort_and_rectify.py
+++ camera_undistort_and_rectify.py
@@ -1,5 +1,5 @@
-from "camera_disp_unsort.py"  import capture_and_undistort
-from "apriltag_homography.py" import rectify_image_with_apriltag
+import camera_disp_undistort
+import apriltag_homography
 if __name__ == '__main__':
-    capture_and_undistort("img_udist.png")
-    rectify_image_with_apriltag("img_udist.png")
+    camera_disp_undistort.capture_and_undistort("img_udist.png")
+    apriltag_homography.rectify_image_with_apriltag("img_udist.png")
