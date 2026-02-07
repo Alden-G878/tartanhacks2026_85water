ea9159e76d9d68ad27f606c5c08a266c973a36ee
Alden G <aldengrover@gmail.com>
2026-02-06 23:28:21 -0500

    Added servo control

--- 85water_teensy/src/main.cpp
+++ 85water_teensy/src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <Servo.h>
 
 enum rx {
   servo_num,
@@ -6,9 +7,19 @@ enum rx {
 };
 
 rx state;
+
+Servo s0, s1, s2, s3, s4, s5, s6;
+
 void setup() {
   Serial1.begin(115200);
   state = servo_num;
+  s0.attach(-1, 1500, 1900);
+  s1.attach(-1, 1500, 1900);
+  s2.attach(-1, 1500, 1900);
+  s3.attach(-1, 1500, 1900);
+  s4.attach(-1, 1500, 1900);
+  s5.attach(-1, 1500, 1900);
+  s6.attach(-1, 1500, 1900);
 }
 
 void loop() {
@@ -23,8 +34,33 @@ void loop() {
         break;
       case command:
         cmd = x;
+        switch(snum) {
+          case 0:
+            s0.writeMicroseconds(cmd);
+            break;
+          case 1:
+            s1.writeMicroseconds(cmd);
+            break;
+          case 2:
+            s2.writeMicroseconds(cmd);
+            break;
+          case 3:
+            s3.writeMicroseconds(cmd);
+            break;
+          case 4:
+            s4.writeMicroseconds(cmd);
+            break;
+          case 5:
+            s5.writeMicroseconds(cmd);
+            break;
+          case 6:
+            s6.writeMicroseconds(cmd);
+            break;
+          default:
+        }
         state = servo_num;
         break;
+      default:
     }
   }
 }
