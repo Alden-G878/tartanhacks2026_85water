5054671b6b7b4592ff3f1d21d1d0ffcc5960deed
Alden G <aldengrover@gmail.com>
2026-02-07 16:40:59 -0500

    firmware work

--- 85water_teensy/src/main.cpp
+++ 85water_teensy/src/main.cpp
@@ -8,32 +8,45 @@ enum rx {
 
 rx state;
 
-Servo s0, s1, s2, s3, s4, s5, s6;
+Servo s0, s1, s2, s3, s4;
+
+int remap(int angle) {
+  int source_span = 199;
+  int source_min = 0;
+  int target_span = 400;
+  int target_min = 1500;
+  double scale_factor = (double)target_span / (double)source_span;
+  return (int)(target_min + (angle-source_min) * scale_factor);
+}
 
 void setup() {
   Serial1.begin(115200);
   state = servo_num;
-  s0.attach(-1, 1500, 1900);
-  s1.attach(-1, 1500, 1900);
-  s2.attach(-1, 1500, 1900);
-  s3.attach(-1, 1500, 1900);
-  s4.attach(-1, 1500, 1900);
-  s5.attach(-1, 1500, 1900);
-  s6.attach(-1, 1500, 1900);
-}
+  s0.attach(24, 1500, 1900);
+  s1.attach(28, 1500, 1900);
+  s2.attach(37, 1500, 1900);
+  s3.attach(36, 1500, 1900);
+  s4.attach(33, 1500, 1900);
+  }
 
 void loop() {
   int x = Serial1.read();
   int snum = -1;
   int cmd = -1;
-  if(x!=-1) {
+  s0.write(0);
+  s1.write(0);
+  s2.write(0);
+  s3.write(0);
+  s4.write(0);
+  /*if(x!=-1) {
     switch(state) {
       case servo_num:
         snum = x;
         state = command;
         break;
       case command:
-        cmd = x;
+        cmd = remap(x);
+        Serial.printf("commanding %i microseconds\n", cmd);
         switch(snum) {
           case 0:
             s0.writeMicroseconds(cmd);
@@ -50,17 +63,11 @@ void loop() {
           case 4:
             s4.writeMicroseconds(cmd);
             break;
-          case 5:
-            s5.writeMicroseconds(cmd);
-            break;
-          case 6:
-            s6.writeMicroseconds(cmd);
-            break;
           default:
         }
         state = servo_num;
         break;
       default:
     }
-  }
+  }*/
 }
